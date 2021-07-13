7e9d6d6cf1982ad64f100e9f897b8b15e15d2a50
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-07-13 15:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello.......... 01'
+    return 'Hello world'
 
 
 @app.errorhandler(500)

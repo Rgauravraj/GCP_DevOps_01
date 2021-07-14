a44da239973f00bd47b1bb14f10d86f095ec926d
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-07-14 16:23:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-#!/usr/bin/env python
+import logging
 
-import webapp2
+from flask import Flask
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self)
-        self.response.write('Hello world')
-        
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_flex_quickstart]
